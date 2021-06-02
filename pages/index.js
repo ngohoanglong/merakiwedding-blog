a4aa13575bc75c37bf9b3d479b66919ed9c0cb73
ngohoanglong <ngohoanglongptit@gmail.com>
2021-06-02 15:01:44 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -27,5 +27,6 @@ export async function getStaticProps({ preview = false }) {
   const allPosts = await getAllPostsForHome(preview)
   return {
     props: { allPosts, preview },
+    revalidate: 300
   }
 }
