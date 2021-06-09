252e416a15e6aabf5858485b0a4d87d43663faa0
Hieu Nguyen <hieunguyenel@gmail.com>
2021-06-09 15:09:13 +0700

    get-all-gallery.js api

---
+++ lib/db/mysqlhandler.ts
@@ -0,0 +1,24 @@
+import mysql from 'serverless-mysql'
+
+export const mysqlHandler = mysql({
+  config: {
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    port: parseInt(process.env.MYSQL_PORT),
+  },
+})
+
+export async function query(
+  q: string,
+  values: (string | number)[] | string | number = []
+) {
+  try {
+    const results = await mysqlHandler.query(q, values)
+    await mysqlHandler.end()
+    return results
+  } catch (e) {
+    throw Error(e.message)
+  }
+}
