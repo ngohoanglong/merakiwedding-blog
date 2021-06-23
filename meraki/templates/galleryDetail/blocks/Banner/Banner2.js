71ce161eb58c6052570d7d8b4aa0e9f1fe5cc04f
ngohoanglong <ngohoanglongptit@gmail.com>
2021-06-23 10:56:04 +0700

    update

--- meraki/templates/galleryDetail/blocks/Banner/Banner2.js
+++ meraki/templates/galleryDetail/blocks/Banner/Banner2.js
@@ -13,7 +13,7 @@ const Banner2 = ({ image = {}, image2 = {}, title, subTitle, details = [] }) => 
           <div className="text-xl md:text-2xl">{subTitle}</div>
           <h1 className="leading-none text-3xl  md:text-5xl font-kinfolk pt-6 max-w-sm">{title}</h1>
           <ul className="py-12 md:text-lg space-y-3" >
-            {details.map((item, i) => (<li key={i} className="flex items-baseline"><div className="font-extrabold truncate">{item.label}</div><div>{item.value}</div></li>))}
+            {details.map((item, i) => (<li key={i} className="flex items-baseline space-x-2"><div className="font-extrabold truncate">{item.label}</div><div>{item.value}</div></li>))}
           </ul>
         </Container>
         <div style={{ zIndex: '-1' }} className='hidden h-full w-1/4 relative flex-shrink-0 md:flex flex-col justify-center'>
