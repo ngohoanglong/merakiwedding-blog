139849379a511e856fb04d8fae961006afa5903a
ngohoanglong <ngohoanglongptit@gmail.com>
2021-06-23 13:10:56 +0700

    update

--- meraki/templates/galleryDetail/blocks/Banner/Banner3.js
+++ meraki/templates/galleryDetail/blocks/Banner/Banner3.js
@@ -1,17 +1,17 @@
 import Container from '@components/container';
 import React from 'react';
 const Banner3 = ({ title, subTitle, details, }) => {
-  return <div className="relative flex w-full items-center justify-center px-12" >
+  return <div className="relative text-center md:text-left flex flex-col md:flex-row py-12 w-full items-center justify-center " >
     <div className="">
       <Container>
-        <div className="text-xl md:text-2xl">{subTitle}</div>
-        <h1 className="leading-none text-3xl  md:text-5xl font-kinfolk pt-6 max-w-sm">{title}</h1>
+        <div className="text-lg md:text-2xl">{subTitle}</div>
+        <h1 className="leading-none text-xl  md:ext-3xl  md:text-5xl font-kinfolk pt-6 md:pt-6 max-w-sm">{title}</h1>
       </Container>
     </div>
-    <div className="">
+    <div className="md:px-12">
       <Container>
-        <ul className="py-12 md:text-lg space-y-5">
-          {details.map((item, i) => (<li key={i}><div className="font-extrabold truncate">{item.label}</div><div>{item.value}</div></li>))}
+        <ul className="py-8 md:py-12  md:text-lg space-y-2 lg:space-y-5">
+          {details.map((item, i) => (<li key={i} className="flex md:flex-col flex-wrap justify-center truncate"><div className="font-extrabold ">{item.label}</div><div className="px-2">{item.value}</div></li>))}
         </ul>
       </Container>
     </div>
