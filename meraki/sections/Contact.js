c66df5d72590c8cda069a5dbb9b059880f6c0eb8
ngohoanglong <ngohoanglongptit@gmail.com>
2021-07-06 09:47:06 +0700

    update

--- meraki/sections/Contact.js
+++ meraki/sections/Contact.js
@@ -13,8 +13,8 @@ export const Contact = () => {
             <div style={{ paddingTop: `${2428 / 5760 * 100}%` }}></div>
             <Image {...get('app.data.contact.image', { src: '/logo.png' })}></Image>
             <div className="absolute inset-0 flex text-white flex-col justify-center items-center text-center p-12">
-              <div className="leading-none font-garamond italic text-xl font-semibold">{get('app.data.contact.subTitle',)}</div>
-              <div className='h-5'></div>
+              {/* <div className="leading-none font-garamond italic text-xl font-semibold">{get('app.data.contact.description',)}</div>
+              <div className='h-5'></div> */}
               <div className="leading-none text-4xl font-kinfolk">{get('app.data.contact.title',)}</div>
               <div className='h-6'></div>
               <div className='flex justify-center'>
@@ -33,7 +33,7 @@ export const Contact = () => {
             <div style={{ paddingTop: `${2428 / 5760 * 100}%` }}></div>
             <Image {...get('app.data.contact.image', { src: '/logo.png' })}></Image>
             <div className="absolute inset-0 text-element-2 flex flex-col justify-center items-center text-center p-12">
-              <div className="leading-none font-garamond italic text-3xl">{get('app.data.contact.subTitle',)}</div>
+              <div className="leading-none font-garamond italic text-3xl">{get('app.data.contact.description',)}</div>
               <div className='h-5'></div>
               <div className="leading-none text-5xl font-kinfolk">{get('app.data.contact.title',)}</div>
               <div className='h-6'></div>
