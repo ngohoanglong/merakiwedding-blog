f8daa9b9c5529f968f923a6aa99e856f1e05f90c
ngohoanglong <ngohoanglongptit@gmail.com>
2021-07-12 17:04:20 +0700

    update

--- meraki/components/Image.js
+++ meraki/components/Image.js
@@ -7,27 +7,27 @@ const breakpoints = {
   small: 500,
   // xsmall: 64
 }
-// const strapiLoader = ({ src, width, quality }) => {
+const strapiLoader = ({ src, width, quality }) => {
 
-//   let size
-//   switch (true) {
-//     case width < breakpoints.small:
-//       size = 'small'
-//       break;
-//     case width < breakpoints.medium:
-//       size = 'medium'
-//       break;
-//     case width < breakpoints.large:
-//       size = 'large'
-//     default:
-//       break;
-//   }
-//   if (!size) return src
-//   return src.replace('/uploads/', '/uploads/' + size + '_')
-// }
+  let size = "large"
+  switch (true) {
+    case width < breakpoints.small:
+      size = 'small'
+      break;
+    case width < breakpoints.medium:
+      size = 'medium'
+      break;
+    case width < breakpoints.large:
+      size = 'large'
+    default:
+      break;
+  }
+  if (!size) return src
+  return src.replace('/uploads/', '/uploads/' + size + '_')
+}
 export const Image = ({ src, alt = "", variant, priority = 100, placeholder = "blur", ...rest }) => {
   let sizes = "(max-width: 400px) 300px, 800px";
-  const enable = src && src.includes('strapi.merakiweddingplanner.com/uploads/')
+  const enable = src && src.includes('strapi.merakiweddingplanner.com/uploads/') && variant !== "cover"
   switch (variant) {
     case 'cover':
       sizes = "(max-width: 400px) 800px, 1400px";
@@ -39,13 +39,13 @@ export const Image = ({ src, alt = "", variant, priority = 100, placeholder = "b
       sizes = "(max-width: 400px) 400px, 800px";
       break;
     default:
-      sizes = "(max-width: 400px) 800px, 1400px";
+      sizes = "(max-width: 400px) 800px, 800px";
       break;
   }
   return (
     <NextImage
       key={src}
-      // loader={enable ? strapiLoader : undefined}
+      loader={enable ? strapiLoader : undefined}
       layout="fill"
       src={src && src || '/logo.png'}
       alt={alt || 'Meraki Image'}
