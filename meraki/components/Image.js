7c9f69378ea7bbde36dcafcbd4d0492f00aca693
ngohoanglong <ngohoanglongptit@gmail.com>
2021-07-13 02:51:57 +0700

    update

--- meraki/components/Image.js
+++ meraki/components/Image.js
@@ -1,4 +1,5 @@
-import NextImage from "next/image";
+import { getThumb } from '@providers/tinacms/helpers'
+import NextImage from 'next/image'
 const breakpoints = {
   // xlarge: 1920,
   large: 1600,
@@ -6,55 +7,72 @@ const breakpoints = {
   small: 500,
   // xsmall: 64
 }
-const strapiLoader = ({ src, width, quality }) => {
+// const strapiLoader = ({ src, width, quality }) => {
 
-  let size = "large"
-  switch (true) {
-    case width < breakpoints.small:
-      size = 'small'
-      break;
-    case width < breakpoints.medium:
-      size = 'medium'
-      break;
-    case width < breakpoints.large:
-      size = 'large'
-    default:
-      break;
-  }
-  if (!size) return src
-  return src.replace('/uploads/', '/uploads/' + size + '_')
-}
-
-export const Image = ({ src, alt = "Meraki wedding planner image", variant, placeholder = "blur", ...rest }) => {
-  let sizes = "(min-width: 1100px) 535px, (min-width: 750px) calc((100vw - 130px) / 2), calc((100vw - 50px) / 2)";
-  // const enable = src && src.includes('strapi.merakiweddingplanner.com/uploads/')
+//   let size
+//   switch (true) {
+//     case width < breakpoints.small:
+//       size = 'small'
+//       break;
+//     case width < breakpoints.medium:
+//       size = 'medium'
+//       break;
+//     case width < breakpoints.large:
+//       size = 'large'
+//     default:
+//       break;
+//   }
+//   if (!size) return src
+//   return src.replace('/uploads/', '/uploads/' + size + '_')
+// }
+export const Image = ({
+  src,
+  alt,
+  variant = 'card-large',
+  priority,
+  placeholder = 'blur',
+  ...rest
+}) => {
+  let sizes
+  // '(max-width: 400px) 300px, 800px'
+  // const enable =
+  //   src &&
+  //   src.includes(
+  //     'strapi.merakiweddingplanner.com/uploads/'
+  //   )
   switch (variant) {
     case 'cover':
-      sizes = "(min-width: 1100px) 100vw, 100vw";
-      break;
+      sizes =
+        '(max-width: 384px) 100vw, 100vw'
+      break
     case 'card':
-      sizes = "(min-width: 1100px) 535px, (min-width: 750px) calc((100vw - 130px) / 2), calc((100vw - 50px) / 2)";
-      break;
+      sizes =
+        '(max-width: 384px) 400px, 600px'
+      break
     case 'card-large':
-      sizes = "(min-width: 1100px) 535px, (min-width: 750px) calc((100vw - 130px) / 2), calc((100vw - 50px) / 2)";
-      break;
+      sizes =
+        '(max-width: 384px) 400px,(max-width: px) 500px,(max-width: 1200px) 960px, 960px'
+      break
     default:
-      sizes = "(min-width: 1100px) 535px, (min-width: 750px) calc((100vw - 130px) / 2), calc((100vw - 50px) / 2)";
-      break;
+      sizes =
+        '(max-width: 384px) 800px, 1400px'
+      break
   }
   return (
     <NextImage
       key={src}
-      // loader={enable ? variant !== "cover" ? strapiLoader : coverLoader : undefined}
+      // loader={enable ? strapiLoader : undefined}
       layout="fill"
-      src={src && src || '/logo.png'}
+      src={(src && src) || '/logo.png'}
       alt={alt || 'Meraki Image'}
       sizes={sizes}
       placeholder={src && placeholder}
-      blurDataURL={placeholder && "/bg_blog.png"}
+      blurDataURL={
+        placeholder && getThumb(src)
+      }
       objectFit="cover"
-      quality={85}
       objectPosition="bottom center"
-      {...rest} />
-  );
-};
+      {...rest}
+    />
+  )
+}
--- meraki/components/XS.js
+++ meraki/components/XS.js
@@ -1,12 +1,18 @@
-import { useSource } from "@providers/source";
-import classNames from 'classnames';
-import { cloneElement } from "react";
+import { useSource } from '@providers/source'
+import classNames from 'classnames'
+import { cloneElement } from 'react'
 
 export const XS = ({ children }) => {
-  const { get } = useSource();
-  const element = children((path, ...args) => get(`${path}.xs`, ...args));
+  const { get } = useSource()
+  const element = children(
+    (path, ...args) =>
+      get(`${path}.xs`, ...args)
+  )
   return cloneElement(element, {
     ...element.props,
-    className: classNames(element.props.className, "lg:hidden")
-  });
-};
+    className: classNames(
+      element.props.className,
+      'lg:hidden'
+    ),
+  })
+}
