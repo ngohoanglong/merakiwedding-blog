fbd9a4ff4a917b607e54ac2ae0e698685e8e400c
ngohoanglong <ngohoanglongptit@gmail.com>
2021-07-15 15:40:04 +0700

    update

--- components/meta.js
+++ components/meta.js
@@ -1,33 +1,31 @@
+import seo from 'data/seo.js'
+import { DefaultSeo } from 'next-seo'
 import Head from 'next/head'
-
 export default function Meta() {
   return (
-    <Head>
-      <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-      <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-      <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/manifest.json" />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-      <meta name="theme-color" content="#ffffff" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <title>Meraki Wedding Planner</title>
-      <link rel="shortcut icon" href="/favicon.png" />
-      <meta
-        name="description"
-        content={`DESTINATION WEDDING PLANNER in Vietnam Independence &#8211; Attentiveness &#8211; Dedication Every wedding crafted by Meraki is an unique experience that was delivered beautifully our Gallery Browse our weddings for tips and inspiration. From Ha Long Bay to Ho Chi Minh city, let us inspire you Mai &#038; Chris Phong &#038; Hoang Tess &#038; Andy Lucia &#038; &hellip; Home Read More &raquo;`}
-      />
-      <meta property="og:image" content={"/home/banner/banner-1.jpg"} />
-    </Head>
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <DefaultSeo {...seo} />
+    </>
   )
 }
---
+++ data/seo.js
@@ -0,0 +1,33 @@
+// homepage: Meraki Wedding Planner | Destination Wedding Planner in Vietnam
+// blog page: Meraki Wedding Planner | Destination Wedding Planner in Vietnam | Blog
+// gallery: Meraki Wedding Planner | Destination Wedding Planner in Vietnam | Journal
+// gallery detail: gallery: Meraki Wedding Planner | Destination Wedding Planner in Vietnam | [ tên couple ]
+// vd: Meraki Wedding Planner | Destination Wedding Planner in Vietnam | Phong & Hoang
+// About Us: Meraki Wedding Planner | Destination Wedding Planner in Vietnam | About Us
+// Kind Word: Meraki Wedding Planner | Destination Wedding Planner in Vietnam | Kind Words
+// Contact: Meraki Wedding Planner | Destination Wedding Planner in Vietnam | Contact
+// Service: Meraki Wedding Planner | Destination Wedding Planner in Vietnam | Service
+
+const title = "Meraki Wedding Planner | Destination Wedding Planner in Vietnam"
+const description = `DESTINATION WEDDING IN VIETNAM Meraki [may-rah-kee]: in Greek means doing something with soul, creativity and love; to put something of yourself into your work.`
+export default {
+  "title": title,
+  "titleTemplate": "Meraki Wedding Planner | Destination Wedding Planner in Vietnam - %s",
+  "description": description,
+  "openGraph": {
+    title,
+    description,
+    "type": "website",
+    "locale": "vi_VN",
+    "url": "https://merakiweddingplanner.com",
+    "site_name": title,
+    "images": [
+      {
+        "url": "https://merakiweddingplanner.com/home/banner/banner-1.jpg",
+        "width": 800,
+        "height": 600,
+        "alt": title
+      }
+    ]
+  }
+}
