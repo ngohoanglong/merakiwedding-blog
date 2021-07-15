e0f2763c1b738705986ef9749846fb1488ef908f
Hieu Nguyen <hieunguyenel@gmail.com>
2021-07-15 13:40:58 +0700

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,24 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+          <Head>
+              <script
+                  async
+                  src="https://www.googletagmanager.com/gtag/js?id=G-152J4BFGVX"
+              />
+
+              <script
+                  dangerouslySetInnerHTML={{
+                      __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                    
+                      gtag('config', 'G-152J4BFGVX');
+            `,
+                  }}
+              />
+          </Head>
         <body>
           <Main />
           <NextScript />
