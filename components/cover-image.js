5bad6834452b687d7a57ede2ffad8cf918b6153e
ngohoanglong <ngohoanglongptit@gmail.com>
2021-07-29 11:09:02 +0700

    fix font bold

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,21 +1,33 @@
 import cn from 'classnames'
 import Image from 'next/image'
 
-export default function CoverImage({ title, coverImage, href, slug }) {
-  const image = (
-    <Image
-      layout='responsive'
-      height={371} width={557}
-      src={coverImage?.sourceUrl}
-      className={cn('shadow-small', {
-        'hover:shadow-medium transition-shadow duration-200 bg-accent-2': slug,
-      })}
-    />
-  )
+export default function CoverImage({
+  title,
+  coverImage,
+  href,
+  slug,
+}) {
+  const image =
+    coverImage?.sourceUrl ? (
+      <Image
+        layout="responsive"
+        height={371}
+        width={557}
+        src={coverImage?.sourceUrl}
+        className={cn('shadow-small', {
+          'hover:shadow-medium transition-shadow duration-200 bg-accent-2':
+            slug,
+        })}
+      />
+    ) : null
   return (
     <div className="sm:mx-0">
       {href ? (
-        <a href={href} aria-label={title}>{image}</a>
+        <a
+          href={href}
+          aria-label={title}>
+          {image}
+        </a>
       ) : (
         image
       )}
