5a102452033491cc95172b7e1386bbae82dafcab
ngohoanglong <ngohoanglongptit@gmail.com>
2021-07-29 11:34:49 +0700

    update font

--- meraki/templates/postDetail/PostDetail.js
+++ meraki/templates/postDetail/PostDetail.js
@@ -1,99 +1,183 @@
-
-import Layout from "@components/layout";
-import PostBody from '@components/post-body';
-import SourceProvider from "@providers/source";
-import get from "lodash.get";
-import { Image } from "meraki/components/Image";
-import LoadingDots from "meraki/components/LoadingDots";
-import Seo from "meraki/components/Seo";
-import Head from "next/head";
-import { useRouter } from "next/router";
-const FixFooterBackgroundColor = () => <style>
-  {
-    `
+import Layout from '@components/layout'
+import PostBody from '@components/post-body'
+import SourceProvider from '@providers/source'
+import get from 'lodash.get'
+import { Image } from 'meraki/components/Image'
+import LoadingDots from 'meraki/components/LoadingDots'
+import Seo from 'meraki/components/Seo'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+const FixFooterBackgroundColor = () => (
+  <style>
+    {`
       footer{
         background:transparent!important
       }
       
-    `
-  }
-</style>
-const PostDetail = ({ source, preview }) => {
+    `}
+  </style>
+)
+const PostDetail = ({
+  source,
+  preview,
+}) => {
   const post = get(source, 'post', {})
   const router = useRouter()
-  return <SourceProvider source={{
-    en: source
-  }}>
-    <Layout preview={preview}>
-
-      <FixFooterBackgroundColor />
-      <div className="max-w-6xl px-2 lg:px-0 mx-auto">
-        {router.isFallback ? (
-          <div className="fixed inset-0 flex justify-center items-center"><LoadingDots /></div>
-        ) : (
-          <>
-            <article>
-              <Seo title={
-                post.title
-              }
-                openGraph={{
-                  title: post.title,
-                  url: 'https://merakiweddingplanner.com/posts/' + get(router, 'query.slug'),
-                  "images": [
-                    {
-                      "url": post.featuredImage?.node?.sourceUrl,
-                      "width": 800,
-                      "height": 600,
-                      "alt": post.title
+  return (
+    <SourceProvider
+      source={{
+        en: source,
+      }}>
+      <Layout preview={preview}>
+        <FixFooterBackgroundColor />
+        <div className="max-w-6xl px-2 lg:px-0 mx-auto">
+          {router.isFallback ? (
+            <div className="fixed inset-0 flex justify-center items-center">
+              <LoadingDots />
+            </div>
+          ) : (
+            <>
+              <article>
+                <Seo
+                  title={post.title}
+                  openGraph={{
+                    title: post.title,
+                    url:
+                      'https://merakiweddingplanner.com/posts/' +
+                      get(
+                        router,
+                        'query.slug'
+                      ),
+                    images: [
+                      {
+                        url: post
+                          .featuredImage
+                          ?.node
+                          ?.sourceUrl,
+                        width: 800,
+                        height: 600,
+                        alt: post.title,
+                      },
+                    ],
+                  }}
+                />
+                <Head>
+                  <link
+                    rel="stylesheet"
+                    id="elementor-icons-css"
+                    href="https://merakiweddingplanner.com/wp-content/themes/astra/assets/css/minified/style.min.css?ver=3.4.8"
+                    media="all"
+                  />
+                  <link
+                    rel="stylesheet"
+                    id="elementor-icons-css"
+                    href="https://merakiweddingplanner.com/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.11.0"
+                    media="all"
+                  />
+                  <link
+                    rel="stylesheet"
+                    id="elementor-animations-css"
+                    href="https://merakiweddingplanner.com/wp-content/plugins/elementor/assets/lib/animations/animations.min.css?ver=3.2.4"
+                    media="all"
+                  />
+                  <link
+                    rel="stylesheet"
+                    id="elementor-frontend-legacy-css"
+                    href="https://merakiweddingplanner.com/wp-content/plugins/elementor/assets/css/frontend-legacy.min.css?ver=3.2.4"
+                    media="all"
+                  />
+                  <link
+                    rel="stylesheet"
+                    id="elementor-frontend-css"
+                    href="https://merakiweddingplanner.com/wp-content/plugins/elementor/assets/css/frontend.min.css?ver=3.2.4"
+                    media="all"
+                  />
+                  <link
+                    rel="stylesheet"
+                    id={
+                      `elementor-post-` +
+                      post.databaseId +
+                      `-css`
                     }
-                  ]
-                }}
-              />
-              <Head>
-                <link rel='stylesheet' id='elementor-icons-css' href='https://merakiweddingplanner.com/wp-content/themes/astra/assets/css/minified/style.min.css?ver=3.4.8' media='all' />
-                <link rel='stylesheet' id='elementor-icons-css' href='https://merakiweddingplanner.com/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.11.0' media='all' />
-                <link rel='stylesheet' id='elementor-animations-css' href='https://merakiweddingplanner.com/wp-content/plugins/elementor/assets/lib/animations/animations.min.css?ver=3.2.4' media='all' />
-                <link rel='stylesheet' id='elementor-frontend-legacy-css' href='https://merakiweddingplanner.com/wp-content/plugins/elementor/assets/css/frontend-legacy.min.css?ver=3.2.4' media='all' />
-                <link rel='stylesheet' id='elementor-frontend-css' href='https://merakiweddingplanner.com/wp-content/plugins/elementor/assets/css/frontend.min.css?ver=3.2.4' media='all' />
-                <link rel='stylesheet' id={`elementor-post-` + post.databaseId + `-css`} href={`https://merakiweddingplanner.com/wp-content/uploads/elementor/css/post-` + post.databaseId + `.css?ver=1625218922`} media='all' />
-                <link rel='stylesheet' href={`https://merakiweddingplanner.com/wp-content/uploads/elementor/css/post-6343.css?ver=1626326961`} media='all' />
+                    href={
+                      `https://merakiweddingplanner.com/wp-content/uploads/elementor/css/post-` +
+                      post.databaseId +
+                      `.css?ver=1625218922`
+                    }
+                    media="all"
+                  />
+                  <link
+                    rel="stylesheet"
+                    href={`https://merakiweddingplanner.com/wp-content/uploads/elementor/css/post-6343.css?ver=1626326961`}
+                    media="all"
+                  />
 
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-                <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap" rel="stylesheet"></link>
-                <link href="https://fonts.googleapis.com/css2?family=Commissioner&display=swap" rel="stylesheet"></link>
-                <style>
-                  {
-                    `
+                  <link
+                    rel="preconnect"
+                    href="https://fonts.googleapis.com"
+                  />
+                  <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin
+                  />
+                  <link
+                    href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+                    rel="stylesheet"
+                  />
+                  <link
+                    href="https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap"
+                    rel="stylesheet"></link>
+                  <link
+                    href="https://fonts.googleapis.com/css2?family=Commissioner&display=swap"
+                    rel="stylesheet"></link>
+                  <link
+                    href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@0;1&display=swap"
+                    rel="stylesheet"
+                  />
+                  <style>
+                    {`
                       .entry-title a, .entry-content h1, h2, .entry-content h2, h3, .entry-content h3, h4, .entry-content h4, h5, .entry-content h5, h6, .entry-content h6 {
                         color: #3a3a3a;
                       }
-                    `
-                  }
-                </style>
-              </Head>
-              <div style={{ zIndex: "-1" }} className="fixed inset-0">
-                <Image src={get(source, 'data.background_image', "/bg_blog.JPG")} />
-              </div>
-              <div className=" pb-12 lg:py-12 lg:pb-32">
-                <div>
-                  <div className="py-6 bg-white rounded lg:py-24 elementor-kit-6343">
-                    <PostBody content={post.content} />
+                    `}
+                  </style>
+                </Head>
+                <div
+                  style={{
+                    zIndex: '-1',
+                  }}
+                  className="fixed inset-0">
+                  <Image
+                    src={get(
+                      source,
+                      'data.background_image',
+                      '/bg_blog.JPG'
+                    )}
+                  />
+                </div>
+                <div className=" pb-12 lg:py-12 lg:pb-32">
+                  <div>
+                    <div className="py-6 bg-white rounded lg:py-24 elementor-kit-6343">
+                      <PostBody
+                        content={
+                          post.content
+                        }
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </article>
-          </>
-        )}
-      </div>
-    </Layout>
-  </SourceProvider>
-
+              </article>
+            </>
+          )}
+        </div>
+      </Layout>
+    </SourceProvider>
+  )
 }
 
 const defaultItem = {
-  background_image: "/bg_blog.JPG"
+  background_image: '/bg_blog.JPG',
 }
 export const postDetail_template = {
   defaultItem,
@@ -103,16 +187,19 @@ export const postDetail_template = {
       name: 'background_image',
       component: 'image',
       // Generate the frontmatter value based on the filename
-      parse: media => process.env.STRAPI_URL + '/uploads/' + media.filename,
+      parse: (media) =>
+        process.env.STRAPI_URL +
+        '/uploads/' +
+        media.filename,
 
       // Decide the file upload directory for the post
       uploadDir: () => '/',
 
       // Generate the src attribute for the preview image.
-      previewSrc: fullSrc => {
-        return fullSrc;
+      previewSrc: (fullSrc) => {
+        return fullSrc
       },
     },
-  ]
-};
+  ],
+}
 export default PostDetail
