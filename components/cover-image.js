9754a1222b496042326443a9559d00c6fcbb00c6
Hieu Nguyen <hieunguyenel@gmail.com>
2021-07-29 18:20:14 +0700

    add image loader and optimize image for blog post

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,6 +1,14 @@
 import cn from 'classnames'
 import Image from 'next/image'
 
+const myLoader = ({ src, width, quality }) => {
+    if (src.indexOf('wp-content') !== -1) {
+        return `https://res.cloudinary.com/dfgbpib38/image/upload/w_500/${src.replace('https://merakiweddingplanner.com/', '')}`;
+    } else {
+        return `/_next/image?url=${src}&w=${width}&q=${quality || 80}`
+    }
+}
+
 export default function CoverImage({
   title,
   coverImage,
@@ -10,6 +18,7 @@ export default function CoverImage({
   const image =
     coverImage?.sourceUrl ? (
       <Image
+          loader={myLoader}
         layout="responsive"
         height={371}
         width={557}
