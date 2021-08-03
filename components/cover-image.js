c5560f15e4b070004a58e4aa4af42b0d4d44f397
ngohoanglong <ngohoanglongptit@gmail.com>
2021-08-03 16:36:54 +0700

    optimize blog image

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,31 +1,24 @@
 import cn from 'classnames'
 import Image from 'next/image'
 
-export default function CoverImage({
-  title,
-  coverImage,
-  href,
-  slug,
-}) {
-  const image =
-    coverImage?.sourceUrl ? (
-      <Image
-        layout="responsive"
-        height={371}
-        width={557}
-        src={coverImage?.sourceUrl}
-        className={cn('shadow-small', {
-          'hover:shadow-medium transition-shadow duration-200 bg-accent-2':
-            slug,
-        })}
-      />
-    ) : null
+export default function CoverImage({ title, coverImage, href, slug }) {
+  const image = coverImage?.sourceUrl ? (
+    <Image
+      layout="responsive"
+      height={371}
+      width={557}
+      alt={title}
+      src={coverImage?.sourceUrl}
+      sizes="(max-width: 640) 640px, 416px"
+      className={cn('shadow-small object-cover', {
+        'hover:shadow-medium transition-shadow duration-200 bg-accent-2': slug,
+      })}
+    />
+  ) : null
   return (
     <div className="sm:mx-0">
       {href ? (
-        <a
-          href={href}
-          aria-label={title}>
+        <a href={href} aria-label={title}>
           {image}
         </a>
       ) : (
