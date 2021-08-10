d62dd62d203424dbaed61b4657b714c3c01fb88b
Hieu Nguyen <hieunguyenel@gmail.com>
2021-08-10 17:06:52 +0700

    raise image quality

--- components/post-body.js
+++ components/post-body.js
@@ -4,7 +4,7 @@ export default function PostBody({ content }) {
   return (
     <div className={styles.root}>
       <div
-        dangerouslySetInnerHTML={{ __html: content.replace(new RegExp('https://merakiweddingplanner.com/wp-content/uploads/', 'g'), 'https://res.cloudinary.com/dfgbpib38/image/upload/w_800/wp-content/uploads/') }}
+        dangerouslySetInnerHTML={{ __html: content.replace(new RegExp('https://merakiweddingplanner.com/wp-content/uploads/', 'g'), 'https://res.cloudinary.com/dfgbpib38/image/upload/w_1200/wp-content/uploads/') }}
       />
     </div>
   )
