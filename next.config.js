d6a65cf840d3facea6e478267dba1f3308d0e2a0
Hieu Nguyen <hieunguyenel@gmail.com>
2022-07-28 16:13:15 +0700

    add imags domain

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ module.exports = {
       'imageproxy.hieunguyen.dev',
       'scontent.cdninstagram.com',
       'cdninstagram.com',
+      'scontent-mrs2-2.cdninstagram.com',
       'image-proxy.ngohoanglongptit8635.workers.dev',
     ],
     deviceSizes: [640, 828, 1200, 1920],
