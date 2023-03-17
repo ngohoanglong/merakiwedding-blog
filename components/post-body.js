b4c10f7de7eda7fe6d6a89b49b0daf76aebaa6a0
Hieu Nguyen <ngohoanglongptit@gmail.com>
2023-03-17 22:06:16 +0700

    update

--- components/post-body.js
+++ components/post-body.js
@@ -1,10 +1,18 @@
 import styles from './post-body.module.css'
 
 export default function PostBody({ content }) {
+  const finalContent = contentt
+    .replace(
+      new RegExp('https://merakiweddingplanner.com/wp-content/uploads/', 'g'),
+      'https://res.cloudinary.com/dfgbpib38/image/upload/w_1200,f_auto/wp-content/uploads/'
+    )
+    .replaceAll('res.cloudinary.com', 'imageproxy.hieunguyen.dev/api/images')
   return (
     <div className={styles.root}>
       <div
-        dangerouslySetInnerHTML={{ __html: content.replace(new RegExp('https://merakiweddingplanner.com/wp-content/uploads/', 'g'), 'https://res.cloudinary.com/dfgbpib38/image/upload/w_1200,f_auto/wp-content/uploads/') }}
+        dangerouslySetInnerHTML={{
+          __html: finalContent,
+        }}
       />
     </div>
   )
